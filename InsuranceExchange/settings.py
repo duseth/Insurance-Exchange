8767f3381b72880a2c758e879af9752a21984bcb
duseth <ilyas.salimov.07@gmail.com>
2021-07-24 15:30:55 +0300

    feat(settings): добавить настройки для модели и сообщений

--- InsuranceExchange/settings.py
+++ InsuranceExchange/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,12 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Custom user model
+
+AUTH_USER_MODEL = "InsuranceApp.Company"
+
+# Custom message tags for Bootstrap
+MESSAGE_TAGS = {
+    messages.ERROR: "danger"
+}
