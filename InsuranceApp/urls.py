cd5df9ee74173aa01fb0a4b105c4b7cbdabe66dc
duseth <ilyas.salimov.07@gmail.com>
2021-07-24 15:33:41 +0300

    fix(urls): исправить маршрутизацию

--- InsuranceApp/urls.py
+++ InsuranceApp/urls.py
@@ -6,6 +6,7 @@ app_name = "InsuranceApp"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.auth, name="login"),
-    path("register", views.register, name="register")
+    path("login", views.user_login, name="login"),
+    path("register", views.user_register, name="register"),
+    path("logout", views.user_logout, name="logout")
 ]
