383bd24ba3bba9019378d04039c9d5b9b30df104
duseth <ilyas.salimov.07@gmail.com>
2021-07-25 19:12:30 +0300

    feat(forms): добавить формы

--- InsuranceApp/forms.py
+++ InsuranceApp/forms.py
@@ -1,5 +1,6 @@
 from .models import *
 from django import forms
+from datetime import date
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -19,3 +20,44 @@ class RegisterForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+
+class UpdateProfileForm(forms.ModelForm):
+    name = forms.CharField(max_length=200)
+    email = forms.EmailField(max_length=255, widget=forms.EmailInput, disabled=True)
+    phone = forms.CharField(max_length=20)
+    description = forms.Textarea()
+
+    class Meta:
+        model = Company
+        fields = ("name", "email", "phone", "description")
+
+
+class ServiceForm(forms.ModelForm):
+    title = forms.CharField(max_length=200)
+    price = forms.FloatField()
+    coverage_amount = forms.FloatField()
+    description = forms.Textarea()
+    type = forms.ChoiceField(widget=forms.Select, choices=InsuranceType.objects.all().values_list("pk", "name"))
+    validity = forms.ChoiceField(widget=forms.Select, choices=ValidityType.objects.all().values_list("pk", "name"))
+
+    class Meta:
+        model = Service
+        fields = ("title", "description", "coverage_amount", "price", "type", "validity")
+
+    def clean_type(self):
+        return InsuranceType.objects.get(pk=self.cleaned_data["type"])
+
+    def clean_validity(self):
+        return ValidityType.objects.get(pk=self.cleaned_data["validity"])
+
+
+class ResponseForm(forms.ModelForm):
+    full_name = forms.CharField(max_length=200)
+    email = forms.EmailField(widget=forms.EmailInput, max_length=200)
+    phone = forms.CharField(max_length=12)
+    birth_date = forms.DateField(initial=date.today())
+
+    class Meta:
+        model = Response
+        fields = ("full_name", "email", "phone", "birth_date")
